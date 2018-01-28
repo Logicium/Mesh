5af7a403effa44d8066ea259bcf25253ab7a7d86
Logicium <Kisora.thomas@gmail.com>
2018-01-28 01:44:14 -0500

    Awesome custom autocomplete

--- public/tools/Tools.js
+++ public/tools/Tools.js
@@ -11,6 +11,15 @@ Search.prototype = {
   }
 };
 
+var linkedData = {
+    Members: {keys:['members','invitees','guests'],data:function(){return [{value:'kt',label:'KT',icon:'public/images/img1.jpg'}]}},
+    Teams:  {keys:['teams'],data:function(){return [{value:'team a',label:'Team A',icon:'public/images/img1.jpg'}]}},
+    Projects:  {keys:['projects'],data:function(){return [{value:'project x',label:'Project X',icon:'public/images/img1.jpg'}]}},
+    Roles:  {keys:['roles'],data:function(){return [{value:'leader',label:'Leader',icon:'public/images/img1.jpg'}]}},
+    Events:  {keys:['events'],data:function(){return [{value:'event zero',label:'Event Zero',icon:'public/images/img1.jpg'}]}},
+    Multi:  {keys:['to','assignees'],data:function(){return [{value:'group 1',label:'Group 1',icon:'public/images/img1.jpg'}]}},
+}
+
 var InputForm = function(inputs){
   this.inputPanel = $('<div>').addClass('toolPanel col-xs-8 animated fadeInUp').css('padding-left','15px').css('padding-right','15px').css('margin-top','15px');
   this.inputPanel.css('height','100%').css('width','100%');
@@ -18,9 +27,139 @@ var InputForm = function(inputs){
   this.inputForm = $('<div>').addClass("inputForm");
   var self2 = this;
   $.each(inputs,function(inputName){
-    self2.inputPanel.append(
-        input(this).addClass('name-form delay-1 animated fadeInUp text-center')
-    );
+      inputName = this;
+      var newInput = input(this).addClass('name-form delay-1 animated fadeInUp text-center');
+      $.each(linkedData.Members.keys,function(){
+          //console.log(this.toString(),inputName.toLowerCase());console.log(this.toString()==inputName.toLowerCase());
+          if(this.toString()==inputName.toLowerCase()){
+              $(newInput).autocomplete({
+                  source:linkedData.Members.data(),
+                  minLength: 0,
+                  select: function (event, ui) {
+                      $("#search").val(ui.item.label);
+                      return false;
+                  }
+              });
+              $(newInput).data("ui-autocomplete")._renderItem = autocompleteCard;
+              self2.inputPanel.append(
+                  newInput
+              );
+          }else{
+              self2.inputPanel.append(
+                  newInput
+              );
+          }
+
+
+      });
+      $.each(linkedData.Teams.keys,function(){
+          //console.log(this.toString(),inputName.toLowerCase());console.log(this.toString()==inputName.toLowerCase());
+          if(this.toString()==inputName.toLowerCase()){
+              $(newInput).autocomplete({
+                  source:linkedData.Teams.data(),
+                  minLength: 0,
+                  select: function (event, ui) {
+                      $("#search").val(ui.item.label);
+                      return false;
+                  }
+              });
+              $(newInput).data("ui-autocomplete")._renderItem = autocompleteCard;
+              self2.inputPanel.append(
+                  newInput
+              );
+          }else{
+              self2.inputPanel.append(
+                  newInput
+              );
+          }
+      });
+      $.each(linkedData.Projects.keys,function(){
+          //console.log(this.toString(),inputName.toLowerCase());console.log(this.toString()==inputName.toLowerCase());
+          if(this.toString()==inputName.toLowerCase()){
+              $(newInput).autocomplete({
+                  source:linkedData.Projects.data(),
+                  minLength: 0,
+                  select: function (event, ui) {
+                      $("#search").val(ui.item.label);
+                      return false;
+                  }
+              });
+              $(newInput).data("ui-autocomplete")._renderItem = autocompleteCard;
+              self2.inputPanel.append(
+                  newInput
+              );
+          }else{
+              self2.inputPanel.append(
+                  newInput
+              );
+          }
+      });
+      $.each(linkedData.Roles.keys,function(){
+          //console.log(this.toString(),inputName.toLowerCase());console.log(this.toString()==inputName.toLowerCase());
+          if(this.toString()==inputName.toLowerCase()){
+              $(newInput).autocomplete({
+                  source:linkedData.Roles.data(),
+                  minLength: 0,
+                  select: function (event, ui) {
+                      $("#search").val(ui.item.label);
+                      return false;
+                  }
+              });
+              $(newInput).data("ui-autocomplete")._renderItem = autocompleteCard;
+              self2.inputPanel.append(
+                  newInput
+              );
+          }else{
+              self2.inputPanel.append(
+                  newInput
+              );
+          }
+      });
+      $.each(linkedData.Events.keys,function(){
+          //console.log(this.toString(),inputName.toLowerCase());console.log(this.toString()==inputName.toLowerCase());
+          if(this.toString()==inputName.toLowerCase()){
+              $(newInput).autocomplete({
+                  source:linkedData.Events.data(),
+                  minLength: 0,
+                  select: function (event, ui) {
+                      $("#search").val(ui.item.label);
+                      return false;
+                  }
+              });
+              $(newInput).data("ui-autocomplete")._renderItem = autocompleteCard;
+              self2.inputPanel.append(
+                  newInput
+              );
+          }else{
+              self2.inputPanel.append(
+                  newInput
+              );
+          }
+      });
+      $.each(linkedData.Multi.keys,function(){
+          //console.log(this.toString(),inputName.toLowerCase());console.log(this.toString()==inputName.toLowerCase());
+          if(this.toString()==inputName.toLowerCase()){
+              $(newInput).autocomplete({
+                  source:linkedData.Multi.data(),
+                  minLength: 0,
+                  select: function (event, ui) {
+                      $("#search").val(ui.item.label);
+                      return false;
+                  }
+              });
+              $(newInput).data("ui-autocomplete")._renderItem = autocompleteCard;
+              self2.inputPanel.append(
+                  newInput
+              );
+          }else{
+              self2.inputPanel.append(
+                  newInput
+              );
+          }
+      });
+
+
+
   });
   $.each($('.toolCard'),function(){if($(this).find('.toolName').text() == "Send"){$(this).remove()}});
   $('.toolBar').append(new ToolCard("Send",'arrow-right').assemble());
@@ -31,6 +170,14 @@ InputForm.prototype = {
         return $(this.inputPanel);
     }
 };
+
+var autocompleteCard = function(body,item){
+    this.card =  $('<div>', {'data-value': item.label.toLowerCase()}).append(row().append(
+        col(6).append( div().css(Styles.backgroundImage(item.icon)).height('80px').width('100%') ),
+        col(6).append( text(item.label,'black','20px').css('font-family', 'Roboto Condensed, sans-serif'))
+    )).appendTo(body.css('background-color',transparentWhiteHeavy()));
+    return this.card;
+}
 
 var Send = function(){
     this.sendPanel = $('<div>').addClass('toolPanel col-xs-8 text-center animated fadeInUp').css('padding-left','15px').css('padding-right','15px').css('margin-top','15px');
